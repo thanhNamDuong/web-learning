af134cc0d8fbf1f1e4b7b907c5b7d0d77bbf4aaf
Duong Thanh Nam <thanhnam.duong.988@gmail.com>
2018-10-09 17:25:00 +0700

    position css and js

---
+++ javascript-project/js-molliza/Js first steps/ass story/main.js
@@ -0,0 +1,7 @@
+var customnameIput = document.querySelector('#customname');
+var randomizeBtn = document.querySelector('.radomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray() {
+    
+}
